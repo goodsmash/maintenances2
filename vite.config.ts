2840892fcd4f9db96a22520a3e95bf7fc1410259
User <user@example.com>
2024-12-08 22:01:41 -0500

    Prepare for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer(),
+      ],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
